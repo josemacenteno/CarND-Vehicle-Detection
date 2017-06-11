3c9651cebe4cf26c1cf61bb63c7f82aa451ea0ae
Jose Centeno <centeno.jose.manuel@gmail.com>
2017-06-10 20:50:46 -0700

    cleaned file

--- process_video.py
+++ process_video.py
@@ -9,28 +9,18 @@ from pipeline import *
 
 
 
-#frame_counter = 0
 def process_image(image):
-    #global frame_counter
     result = pipeline(image)
-
-    # output_name = "./video_clip/video" + str(frame_counter) + ".jpg"
-    # bgr_image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) 
-    # cv2.imwrite(output_name, bgr_image)
-
-    # cv2.imshow("img", result)
-    # cv2.waitKey(10)
-    #frame_counter += 1
     return result
 
 
-clip1 = VideoFileClip("project_video.mp4")
-#clip1 = VideoFileClip("test_video.mp4")
+#clip1 = VideoFileClip("project_video.mp4")
+clip1 = VideoFileClip("test_video.mp4")
 #clip1 = VideoFileClip("project_video.mp4").subclip(4,24)
-white_clip = clip1.fl_image(process_image) #NOTE: this function expects color images!!
+processed_clip = clip1.fl_image(process_image) #NOTE: this function expects color images!!
 
-white_output = 'video_output.mp4'
-#white_output = 'test_video_output.mp4'
-#white_output = 'clip_video_output.mp4'
+#output_name = 'video_output.mp4'
+output_name = 'test_video_output.mp4'
+#output_name = 'clip_video_output.mp4'
 
-white_clip.write_videofile(white_output, audio=False)
+processed_clip.write_videofile(output_name, audio=False)
