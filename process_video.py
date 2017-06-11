f75c28107a1a422a29e6221a0f12f1819d60698a
Jose Centeno <centeno.jose.manuel@gmail.com>
2017-06-10 20:51:34 -0700

    removed unncesary imports

--- process_video.py
+++ process_video.py
@@ -1,10 +1,5 @@
 #!/home/jcenteno/anaconda3/envs/carnd-term1/bin/python
 from moviepy.editor import VideoFileClip
-from IPython.display import HTML
-import numpy as np
-import cv2
-from moviepy.editor import VideoFileClip
-
 from pipeline import *
 
 
