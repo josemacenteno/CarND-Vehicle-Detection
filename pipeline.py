e139dc50ceb41c7de9fcbc6f12a9d84644a345f4
Jose Centeno <centeno.jose.manuel@gmail.com>
2017-06-12 10:43:28 -0700

    last tweaks. Looking good

--- pipeline.py
+++ pipeline.py
@@ -118,7 +118,7 @@ def get_hog_features(img, orient, pix_per_cell, cell_per_block,
         features, hog_image = hog(img, orientations=orient, 
                                   pixels_per_cell=(pix_per_cell, pix_per_cell),
                                   cells_per_block=(cell_per_block, cell_per_block), 
-                                  transform_sqrt=True, 
+                                  transform_sqrt=False, 
                                   visualise=vis, feature_vector=feature_vec)
         return features, hog_image
     # Otherwise call with one output
@@ -126,7 +126,7 @@ def get_hog_features(img, orient, pix_per_cell, cell_per_block,
         features = hog(img, orientations=orient, 
                        pixels_per_cell=(pix_per_cell, pix_per_cell),
                        cells_per_block=(cell_per_block, cell_per_block), 
-                       transform_sqrt=True, 
+                       transform_sqrt=False, 
                        visualise=vis, feature_vector=feature_vec)
         return features
 
@@ -211,7 +211,8 @@ def pipeline(image, persistance=True, in_color_channel = "RGB"):
     heat = add_heat(heat,hot_windows + windows + small_windows)
         
     # Apply threshold to help remove false positives
-    heat = apply_threshold(heat, min(len(small_windows),len(windows),len(windows_from_prev_cycle)))
+
+    heat = apply_threshold(heat, len(windows_from_prev_cycle))
 
     # Visualize the heatmap when displaying    
     heatmap = np.clip(heat, 0, 255)
--- writeup.md
+++ writeup.md
@@ -49,7 +49,7 @@ I started by reading in all the `vehicle` and `non-vehicle` images.  Here is an 
 
 I experimented on the quiz from class to choose the color space YCrCb, as it gave the best results measured by the SVC model accuracy on the a random training set. 
 
-In the quiz and the project I used  `skimage.hog()` parameters (`orientations`, `pixels_per_cell`, and `cells_per_block`).  I grabbed random images from each of the two classes and displayed them to get a feel for what the `skimage.hog()` output looks like.
+In the quiz and the project I used  `skimage.feature.hog()` parameters (`orientations`, `pixels_per_cell`, and `cells_per_block`).  
 
 Here is an example using the `YCrCb` color space and HOG parameters of `orientations=8`, `pixels_per_cell=(8, 8)` and `cells_per_block=(2, 2)`:
 
